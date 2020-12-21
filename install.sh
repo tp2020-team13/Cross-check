672d04b071eb771d5372ef3d6ae1a87d3f7b7ef1
Simon Misencik <smisencik@frinx.io>
2020-12-21 20:57:58 +0100

    Add config file

--- install.sh
+++ install.sh
@@ -4,18 +4,28 @@ set -e
 
 # Common functions
 function example {
-  echo -e "example: $0"
+  echo -e "example: $0 --full"
 }
 
 function usage {
   echo -e "usage: $0 [OPTIONS]\n"
-  echo -e "Cross-check application installation and deployment script."
+  echo -e "Script for installation and deployment Cross-check application."
+}
+
+function configWarning {
+  echo -e "Please check the config file before installation!"
+  echo -e "ACTUAL CONFIG:"
+  echo -e "Domain: $domain"
 }
 
 function help {
   usage
+  echo -e ""
+  configWarning
+  echo -e ""
     echo -e "OPTIONS:"
     echo -e " -h | --help        Display this message and exit"
+    echo -e " --full             Full installation and deployment"
     echo -e ""
   example
 }
@@ -23,6 +33,7 @@ function help {
 function argumentsCheck {
   if [ $1 -eq 0 ]
   then
+    help
     return
   fi
 
@@ -52,4 +63,5 @@ function argumentsCheck {
 # Script starts here
 # =======================================
 
+. ./config
 argumentsCheck $# $@
