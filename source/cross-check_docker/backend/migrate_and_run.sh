4d7c2afb5aea5bdb20ebeb52e0c6e4465ceab330
Simon Misencik <smisencik@frinx.io>
2021-01-22 21:20:14 +0100

    Add backend docker image

--- source/cross-check_docker/backend/migrate_and_run.sh
+++ source/cross-check_docker/backend/migrate_and_run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-java -cp ./backend.jar com.tp2020.backend.migrator.MigratorKt ./application.production.conf &&
-java -jar ./backend.jar -config=./application.production.conf
+java -cp ./app.jar com.tp2020.backend.migrator.MigratorKt ./application.production.conf &&
+java -jar ./app.jar -config=./application.production.conf
