0544b7ab28b2e7cdef604bcf470115d1b3ff8b54
Šimon Mišenčík <32100265+SimonMisencik@users.noreply.github.com>
2021-04-28 11:36:27 +0200

    Update init-letsencrypt.sh

--- source/cross-check_docker/api_gateway/init-letsencrypt.sh
+++ source/cross-check_docker/api_gateway/init-letsencrypt.sh
@@ -7,7 +7,7 @@ fi
 
 domains=()
 rsa_key_size=4096
-data_path="./data/certbot"
+data_path="../../../../crosscheck_data/certbot_data"
 email="" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
